ea7f0b621fd34d76cc3a944f503d64ad0a81c6c0
sadat <sadat.himel@gmail.com>
2020-09-07 00:38:27 +0600

    done gouting simple site

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function ItemDetail ({match}){
+const ItemDetail=()=> {
 // console.log(match)
   // useEffect(() => {
   //   fetchItem();
   // }, []);
 
-  const [item, setItem] = useState({});
+    let { id } = useParams();
+  const [item, setItem] = useState([]);
+  console.log(item);
 
   // const fetchItem = async () => {
   //   const data = await fetch(
-  //     `https://jsonplaceholder.typicode.com/posts?userId=${match.params.id}`
+  //     `https://jsonplaceholder.typicode.com/posts?userId/${match.params.id}`
   //   );
   //   const items = await data.json();
   //   setItem(item);
@@ -19,17 +22,19 @@ function ItemDetail ({match}){
 
   useEffect(()=> {
     fetch(
-      `https://jsonplaceholder.typicode.com/posts?userId=${match.params.id}`
+      `https://jsonplaceholder.typicode.com/users/${id}`)
       .then(res => res.json())
       .then(data =>{
         setItem(data)
+        
       } )
-    );
   },[])
 
   return (
     <div>
       <h1>name:{item.name}</h1>
+      <h1>email:{item.email}</h1>
+  <p>Phone: {item.phone}</p>
     </div>
   );
 };
